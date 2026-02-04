f76b479a77508fdffcfae9b84af3d123ebb325f2
Alpha Film & Television <info@alphafilmtv.com>
2026-02-04 17:11:39 -0500

    Set dark mode as default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className="h-dvh">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
